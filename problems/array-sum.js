5c8c626ba883b770a1979432d8179830f7f5e7cc
josueahernandez <josueh023@gmail.com>
2015-09-29 10:01:26 -0600

    Here is my quiz.

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof element === 'object') {
+      arr[i] = arraySum(arr[i]);
+    }
+    total += arr[i];
+  }
 
+  return total;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function longestString(arr) {
-
+  var longest;
+  for (var i = 0; i < arr.length; i++) {
+    var elem = arr[i];
+    if (typeof elem === 'string') {
+      if (!longest || elem.length > longest.length) {
+        longest = elem;
+      }
+    }
+  }
+  return longest;
 }
 
 module.exports = longestString;
